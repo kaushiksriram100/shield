3538515e383c782b8529aa1ea733c13d15bad9e5
srkaushi <srkaushi@cisco.com>
2023-02-12 17:48:05 -0800

    scale-up: Add etcd unit test case

--- server/handler_memlookup_test.go
+++ server/handler_memlookup_test.go
@@ -38,3 +38,32 @@ func TestLookUpMalwareDB(t *testing.T) {
 		t.Error("Response Body Does NOT match")
 	}
 }
+
+func TestLookupMalwareEtcD(t *testing.T) {
+
+	testCase := "/urlinfo/1/google.com:8080/search?v=2&name=value"
+	expectedResponse := "{\"url\":\"google.com:8080/search\",\"blacklisted_query_strings\":{\"name\":\"value\",\"v\":\"2\"},\"is_malware_infected\":true}\n"
+
+	router := httprouter.New()
+	router.Handle("GET", "/urlinfo/1/:hostname_with_port/*original_path", LookupMalwareEtcD)
+	rr := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("GET", testCase, nil)
+	router.ServeHTTP(rr, req)
+
+	/*
+		t.Log(req.URL.Path)
+		t.Log(req.URL)
+		t.Log(rr.Code)
+	*/
+
+	//Verify Results
+	if rr.Code != 200 {
+		t.Error("Return Code does not match")
+	}
+	if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(expectedResponse) {
+		t.Log(rr.Body.String())
+		t.Log(expectedResponse)
+		t.Error("Response Body Does NOT match")
+	}
+}
